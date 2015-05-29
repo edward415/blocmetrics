923fffed18fff20ccfa3bd275435a13d590029ea
Ed C <edc415@outlook.com>
2015-05-29 03:29:10 +0000

    finished building API v1

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -4,23 +4,22 @@ class Api::EventsController < ApplicationController
   
   def create
     puts request.env['HTTP_ORIGIN'].inspect
-    # app = App.find_by(url: request.env['HTTP_ORIGIN'])
-    # puts "*" * 10 
-    # puts app.inspect
-    # raise
-    # event = app.events.build(event_params)
-    # if app.nil?
-    #   render json: "Unregistered application", status: :unprocessable_entity
-    # elsif event.save
-    #   render json: @event, status: :created
-    # else
-    #   render @event.errors, status: :unprocessable_entity
-    # end
+    app = App.find_by(url: request.env['HTTP_ORIGIN'])
+    puts "*" * 10 
+    puts app.inspect
+    event = app.events.build(event_params)
+    if app.nil?
+      render json: "Unregistered application", status: :unprocessable_entity
+    elsif event.save
+      render json: @event, status: :created
+    else
+      render @event.errors, status: :unprocessable_entity
+    end
   end
   
   private
   
   def event_params
-    params.require(:event).permit(:name)
+    params.require(:event).permit(:name, :event_name)
   end
 end
